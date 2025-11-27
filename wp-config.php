fd2941018e2e3056d93261b99b202169b9a52c16
Maximiliano Mugnolo <maximiliano.mugnolo@iteam.com.ar>
2025-11-27 13:47:56 -0300

    segundo commit

--- wp-config.php
+++ wp-config.php
@@ -1,15 +1,36 @@
 <?php
-define( 'DB_NAME',     getenv('WORDPRESS_DB_NAME') );
-define( 'DB_USER',     getenv('WORDPRESS_DB_USER') );
-define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
-define( 'DB_HOST',     getenv('WORDPRESS_DB_HOST') . ':3306' );
 
-define( 'DB_CHARSET', 'utf8mb4' );
-define( 'DB_COLLATE', '' );
+// Leer las vars que ECS inyecta
+$db_host = getenv('DB_HOST')     ?: '127.0.0.1';
+$db_name = getenv('DB_DATABASE') ?: 'demo';
+$db_user = getenv('DB_USERNAME') ?: 'laravel';
+$db_pass = getenv('DB_PASSWORD') ?: '';
+$db_port = getenv('DB_PORT')     ?: '3306';
+
+// Config WordPress
+define('DB_NAME',     $db_name);
+define('DB_USER',     $db_user);
+define('DB_PASSWORD', $db_pass);
+// MySQL usa 3306 por defecto, así que podemos no poner el puerto:
+define('DB_HOST',     $db_host); // o "$db_host:$db_port" si querés ser explícito
+
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
 
 $table_prefix = 'wp_';
 
-define( 'WP_DEBUG', false );
+// Logs/errores
+define('WP_DEBUG', false);
+
+// Clave única/salts (podés generar nuevas en https://api.wordpress.org/secret-key/1.1/salt/)
+define('AUTH_KEY',         'poné algo fuerte acá');
+define('SECURE_AUTH_KEY',  'poné algo fuerte acá');
+define('LOGGED_IN_KEY',    'poné algo fuerte acá');
+define('NONCE_KEY',        'poné algo fuerte acá');
+define('AUTH_SALT',        'poné algo fuerte acá');
+define('SECURE_AUTH_SALT', 'poné algo fuerte acá');
+define('LOGGED_IN_SALT',   'poné algo fuerte acá');
+define('NONCE_SALT',       'poné algo fuerte acá');
 
 if (!defined('ABSPATH')) {
     define('ABSPATH', __DIR__ . '/');
